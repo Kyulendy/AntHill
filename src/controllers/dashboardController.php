575ca84f22712ebb47f22ef821802fad4f5870da
Antoine <antoine-bienvenu@sfr.fr>
2013-06-06 15:53:14 +0200

    initialisation de la page d'accueil

---
+++ src/controllers/dashboardController.php
@@ -0,0 +1,7 @@
+<?php 
+
+class dashboardController extends TzController {
+	 public function showAction () {
+		 echo 'Vous &ecirc;tes sur la page : dashboard';
+	}
+}
---
+++ src/controllers/defaultController.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * This class is call by /src/config/routing.yml
+ * when no parameters are passed.
+ * You can change is behavior, do what you want.
+ */
+class defaultController extends TzController {
+
+	// first method call when the website is launched
+	public function showAction () {
+		$this->tzRender->run('/templates/default');                                                        
+	}
+}
